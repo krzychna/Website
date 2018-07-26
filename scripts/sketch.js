7b3b14cf18dc080da4a8e9bc407749a07361e2f3
krzychna <kkrzys17@gmail.com>
2018-07-26 12:17:54 +0200

    Changes incoming

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,31 +1,34 @@
-var myFont;
-var points;
-var vectors;
-
-function preload(){
-  myFont = loadFont('scripts/a.ttf');
-}
-
+/*jshint esversion: 6 */
 function setup() {
   var divCan = document.getElementById('canvas');
   var width = divCan.clientWidth;
-  var canvas = createCanvas(width, 200);
-  canvas.parent('canvas');
-
-  points = myFont.textToPoints('Krzysztof Nowak', width/5, 125, 120, {
-    sampleFactor: 0.1
-  });
-  for (var i = 0; i < points.length; i++) {
-    var curPoint = points[i];
-    stroke(255);
-    point(curPoint.x,curPoint.y);
-    vectors = createVector(curPoint.x,curPoint.y);
-  }
-  console.log(vectors);
+  var height = divCan.clienHeight;
+  var canvas = createCanvas(width,height);
+  background('rgba(51,51,51,0.4)');
 }
 
+var nOfPoints = 100;
+var points = [];
 function draw() {
-    textFont(myFont);
-    background(51);
 
+  if(points.length < nOfPoints){
+    for(let i = 0; i < nOfPoints; i++){
+    points.push(new Punkt(random(i/nOfPoints,width), random(-height,0),i));
     }
+  }
+  while(points.length<nOfPoints){
+
+  }
+
+  if(!(frameCount%2)){
+    for(let i = 0; i<points.length; i++){
+      points[i].draw();
+      points[i].update();
+    }
+  }
+
+  if(!(frameCount%8)){
+      background('rgba(51,51,51,0.4)');
+  }
+
+}
