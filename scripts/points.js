ca3bd3580f6829fea69959059ce97e09ce9b2f00
krzychna <kkrzys17@gmail.com>
2018-07-27 13:13:19 +0200

    Changed everything again

--- scripts/points.js
+++ scripts/points.js
@@ -2,19 +2,30 @@ function Punkt(x,y,id){
   this.x = x;
   this.y = y;
   this.id = id;
-  this.speed = random(1,3);
+  this.speed = random(1,3.2);
+  this.size = random(1,4);
 }
 Punkt.prototype.draw = function(){
-    stroke('rgba(200,200,255,0.6)');
-    strokeWeight(2);
+  strokeWeight(this.size);
+  if(this.speed>3) {
+    stroke('rgba(255,200,200,1)');
+  }
+  else if(this.speed>2) {
+    stroke('rgba(200,200,255,0.8)');
+  }
+  else if(this.speed>1) {
+    stroke('rgba(200,255,200,0.8)');
+  }
     point(this.x,this.y);
   };
 
 Punkt.prototype.update = function(){
   this.y +=this.speed;
-  if(this.y > windowHeight){
+  this.size -= 0.02;
+  if(this.y > windowHeight || this.size < 0.1){
     this.x = random(this.id/100, windowWidth);
-    this.y = random(-windowHeight*0.6,0);
-    this.speed = random(1,3);
+    this.y = random(-windowHeight*0.01,0);
+    this.speed = random(1,4);
+    this.size = random(1,4);
   }
 };
